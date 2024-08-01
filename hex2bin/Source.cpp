6e88c1fb08e23d8961233b0269a43d04d19392b3
Danila <72028641+Myforceisback@users.noreply.github.com>
2024-08-01 23:26:52 +0300

    v0.3

--- hex2bin/Source.cpp
+++ hex2bin/Source.cpp
@@ -5,12 +5,13 @@
 
 int main() {
     setlocale(LC_ALL, "Rus");
-
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     std::string hexFile;
     std::string binFile;
     Hex2bin converter;
 
-    std::cout << "Âûáåðèòå äåéñòâèå:" << std::endl;
+    std::cout << "Ã‚Ã»Ã¡Ã¥Ã°Ã¨Ã²Ã¥ Ã¤Ã¥Ã©Ã±Ã²Ã¢Ã¨Ã¥:" << std::endl;
     std::cout << "1) HEX2BIN" << std::endl;
     std::cout << "2) BIN2HEX" << std::endl;
     std::cout << ">>";
@@ -19,17 +20,17 @@ int main() {
     switch (sw)
     {
     case(1):{
-        std::cout << "Ââåäèòå ïóòü ê *.hex* ôàéëó (path/name.hex):";
+        std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã³Ã²Ã¼ Ãª *.hex* Ã´Ã Ã©Ã«Ã³ (path/name.hex):";
         std::cin >> hexFile;
-        std::cout << "Ââåäèòå ïóòü äëÿ ñîõðàíåíèÿ *.bin* ôàéëà (path/name.bin):";
+        std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã³Ã²Ã¼ Ã¤Ã«Ã¿ Ã±Ã®ÃµÃ°Ã Ã­Ã¥Ã­Ã¨Ã¿ *.bin* Ã´Ã Ã©Ã«Ã  (path/name.bin):";
         std::cin >> binFile;
         std::ifstream inFile(hexFile, std::ios::binary);
         if (!inFile) {
-            std::cerr << "Hex ôàéë íå îòðûëñÿ!" << std::endl;
+            std::cerr << "Hex Ã´Ã Ã©Ã« Ã­Ã¥ Ã®Ã²Ã°Ã»Ã«Ã±Ã¿!" << std::endl;
         }
         std::ofstream outFile(binFile, std::ios::out | std::ios::binary);
         if (!outFile) {
-            std::cerr << "Bin ôàéë íå îòðûëñÿ!" << std::endl;
+            std::cerr << "Bin Ã´Ã Ã©Ã« Ã­Ã¥ Ã®Ã²Ã°Ã»Ã«Ã±Ã¿!" << std::endl;
         }
         converter.intelHexToBin(inFile, outFile);
         inFile.close();
@@ -38,17 +39,17 @@ int main() {
         break;
     }
     case(2): {
-        std::cout << "Ââåäèòå ïóòü ê *.bin* ôàéëó (path/name.bin):";
+        std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã³Ã²Ã¼ Ãª *.bin* Ã´Ã Ã©Ã«Ã³ (path/name.bin):";
         std::cin >> binFile;
-        std::cout << "Ââåäèòå ïóòü äëÿ ñîõðàíåíèÿ *.hex* ôàéëà (path/name.hex):";
+        std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã³Ã²Ã¼ Ã¤Ã«Ã¿ Ã±Ã®ÃµÃ°Ã Ã­Ã¥Ã­Ã¨Ã¿ *.hex* Ã´Ã Ã©Ã«Ã  (path/name.hex):";
         std::cin >> hexFile;
         std::ifstream inFile(binFile, std::ios::binary);
         if (!inFile) {
-            std::cerr << "Bin ôàéë íå îòðûëñÿ!" << std::endl;
+            std::cerr << "Bin Ã´Ã Ã©Ã« Ã­Ã¥ Ã®Ã²Ã°Ã»Ã«Ã±Ã¿!" << std::endl;
         }
         std::ofstream outFile(hexFile, std::ios::out | std::ios::binary);
         if (!outFile) {
-            std::cerr << "Hex ôàéë íå îòðûëñÿ!" << std::endl;
+            std::cerr << "Hex Ã´Ã Ã©Ã« Ã­Ã¥ Ã®Ã²Ã°Ã»Ã«Ã±Ã¿!" << std::endl;
         }
         converter.binToIntelHex(inFile, outFile);
         inFile.close();
